a34b32916d5e9c1d016d6a9d3fcd4df0b3f68059
keenanmacneill <macneill.keenan@gmail.com>
2026-03-19 09:00:08 -0400

    removes timestamps from mgiration

--- migrations/20260318171743_create_pet_type_table.js
+++ migrations/20260318171743_create_pet_type_table.js
@@ -1,11 +1,10 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('pet_type', table => {
-        table.increments(); // adds an auto incrementing PK column
-        table.string('name').notNullable(); // equivalent of varchar(255)
-        table.timestamps(true, true); // utility columns, adds created_at and updated_at
-    });
+  return knex.schema.createTable('pet_type', table => {
+    table.increments(); // adds an auto incrementing PK column
+    table.string('name').notNullable(); // equivalent of varchar(255)
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('pet_type');
+  return knex.schema.dropTableIfExists('pet_type');
 };
