e5723ca79a5f93a68745dce323a867039c8124a0
keenanmacneill <macneill.keenan@gmail.com>
2026-03-23 17:05:36 -0400

    working docker-compose with frontend and backend

--- frontend/api/request.js
+++ frontend/api/request.js
@@ -1,4 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+console.log(import.meta.env.VITE_API_URL);
 
 export async function request(path, options = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,17 @@ import { getAllPets } from '../api/petRequests';
 export default function App() {
   const [pets, setPets] = useState(null);
 
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    getAllPets().then(setPets);
+    getAllPets()
+      .then(data => {
+        setPets(data);
+      })
+      .catch(setError);
   }, []);
 
+  if (error) return <h1>Error: {error.message}</h1>;
   if (!pets) return <h1>Loading...</h1>;
   return (
     <div>
